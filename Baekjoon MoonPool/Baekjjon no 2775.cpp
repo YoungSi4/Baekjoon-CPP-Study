45b53f674a48c69b981c1c192f3c1cd8070a3e20
YoungSi4 <dksdudgns01@gmail.com>
2024-01-26 18:48:46 +0900

    No.2775 - not in cpp

---
+++ Baekjoon MoonPool/Baekjjon no 2775.cpp
@@ -0,0 +1,37 @@
+// no 2775 부녀회장이 될테야
+// 어... 파이썬 쓸게요
+#include<iostream>
+using namespace std;
+
+int main() {
+	/*
+	 내가 5층 5호에 살고 싶어
+	 그러면,
+	 4층 1호 ~ 5호까지 사람의 수 합만큼...
+
+	 0층: 1 2 3 4 5
+	 1층: 1 3 6 10 15
+	 2층: 1 4 10 16 31
+	 3층: 1 5 15 31 62
+	 4층: 1 6 21 52 114
+	 5층: 1 7 28 80 194
+	 ...
+	 Resident[0][i-1] = i
+	 Resident[a][0] = 1
+	 Resident[a][b-1] = Resident[a-1][b] + Resident[a][b-1]
+	 b > 1
+
+	 정적할당 해야하나.
+	 Apartment = [a][b]
+	 */
+	
+	int Apartment
+
+	for(i = 1; i <= b; i++){
+		Apartment[0][i-1] = i	
+	}
+	for(i = 0; i <= a; i++){
+		
+
+	
+}
